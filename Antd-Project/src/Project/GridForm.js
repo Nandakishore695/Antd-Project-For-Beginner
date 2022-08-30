ef1c54563ee52c8b730c6a895c9aced78d9c2ecc
Nandakishore Vishwanath <104244494+Nandakishore695@users.noreply.github.com>
2022-08-30 16:35:04 +0530

    Update GridForm.js

--- Antd-Project/src/Project/GridForm.js
+++ Antd-Project/src/Project/GridForm.js
@@ -1,50 +1,56 @@
 import React from "react";
-import { Button, DatePicker, Form, Input, Row, Col, Select, Radio } from 'antd'
+import { DatePicker, Button, Form, Input, Row, Col, Select, Radio, } from 'antd'
 const { Option } = Select;
 
 class GridFormCom extends React.Component {
+    constructor(){
+        super()
+    }
 
-    validateMessages = () => {
-        console.log();
-    };
     onSave = () => {
-        console.log();
+       
     };
 
     render() {
         return (
             <>
-                <Form validateMessages={this.validateMessages}>
+                <Form onFinish={this.onSave} autoComplete="off" >
                     <Row>
                         <Col>
-                        <br/>
-                            <Form.Item label="First name" style={{ width: "320px" }}
-                                rules={[{ required: true }]}
+                            <br />
+                            <Form.Item
+                                name="firstname"
+                                style={{ width: "320px" }}
+                                rules={[{required: true }]} 
                             >
-                              
+                                <label>First name</label>
                                 <Input placeholder="firstname" />
-                                
                             </Form.Item>
                         </Col>
+                        
                         <Col>
-                        <br/>
-                            <Form.Item label="Last name" style={{ width: "320px" }} >
+                            <br />
+                            <Form.Item name="lastname" style={{ width: "320px" }}
+                            rules={[{required: true }]}  >
+                                <label>Last name</label>
                                 <Input placeholder="lastname" />
                             </Form.Item>
                         </Col>
                         <Col>
-                        <br/>
-                            <Form.Item label="DOB">
-                                <DatePicker />
+                            <br />
+                            <Form.Item name="date of birth" rules={[{required: true }]} >
+                                <label>DOB</label>
+                                <br/>
+                                <DatePicker style={{ width: "320px" }}/>
                             </Form.Item>
                         </Col>
                         <Col>
-                        <br/>
-                            <Form.Item
-                                label="Desgination"
-                                rules={[{ required: true, message: 'Please select your desgination!' }]}
+                            <br />
+                            <Form.Item name="desgination" 
+                                rules={[{ required: true }]}
                                 style={{ width: "320px" }}
                             >
+                                <label>Desgination</label>
                                 <Select placeholder="Please select a desgination">
                                     <Option value="coo">COO</Option>
                                     <Option value="ceo ">CEO </Option>
@@ -52,11 +58,11 @@ class GridFormCom extends React.Component {
                             </Form.Item>
                         </Col>
                         <Col>
-                            <Form.Item
-                                label="Department"
-                                rules={[{ required: true, message: 'Please select your department!' }]}
+                            <Form.Item name="department" 
+                                rules={[{ required: true }]}
                                 style={{ width: "320px" }}
                             >
+                                <label>Department</label>
                                 <Select placeholder="Please select a department">
                                     <Option value="coo">COO</Option>
                                     <Option value="ceo ">CEO </Option>
@@ -64,21 +70,23 @@ class GridFormCom extends React.Component {
                             </Form.Item>
                         </Col>
                         <Col>
-                            <Form.Item label="Email" style={{ width: "320px" }}>
-                                <Input placeholder="email" />
+                            <Form.Item name="email" style={{ width: "320px" }} rules={[{ required: true }]}>
+                                <label>Email</label>
+                                <Input placeholder="email"/>
                             </Form.Item>
                         </Col>
                         <Col>
-                            <Form.Item label="Phone" style={{ width: "320px" }}>
-                                <Input placeholder="phone   " />
+                            <Form.Item name="phone" style={{ width: "320px" }} rules={[{ required: true }]}>
+                                <label>Phone</label>
+                                <Input placeholder="phone" />
                             </Form.Item>
                         </Col>
                         <Col>
-                            <Form.Item
-                                label="Country"
-                                rules={[{ required: true, message: 'Please select your country!' }]}
+                            <Form.Item name="phone"
+                                rules={[{ required: true}]}
                                 style={{ width: "320px" }}
                             >
+                                <label>Country</label>
                                 <Select placeholder="Please select a country">
                                     <Option value="india">India</Option>
                                     <Option value="china ">China </Option>
@@ -86,24 +94,27 @@ class GridFormCom extends React.Component {
                             </Form.Item>
                         </Col>
                         <Col>
-                            <Form.Item
-                                label="State"
-                                rules={[{ required: true, message: 'Please select your state!' }]}
+                            <Form.Item name="state"
+                                rules={[{ required: true}]}
                                 style={{ width: "320px" }}
                             >
-                                <Select placeholder="Please select a state">
+                                <label>State</label>
+                                <Select placeholder="Please select a state" rules={[{ required: true}]}>
                                     <Option value="hyderabad">Hyderabad</Option>
                                     <Option value="delhi ">Delhi</Option>
                                 </Select>
                             </Form.Item>
                         </Col>
                         <Col>
-                            <Form.Item label="City" style={{ width: "320px" }}>
+                            <Form.Item name="city" rules={[{ required: true}]} style={{ width: "320px" }}>
+                                <label>City</label>
                                 <Input placeholder="city" />
                             </Form.Item>
                         </Col>
                         <Col>
-                            <Form.Item label="Gender">
+                            <Form.Item  name="gender" rules={[{ required: true}]} style={{ width: "320px" }}>
+                                <label>Gender</label>
+                                <br />
                                 <Radio.Group>
                                     <Radio value="male"> Male </Radio>
                                     <Radio value="female"> Female </Radio>
@@ -111,17 +122,20 @@ class GridFormCom extends React.Component {
                             </Form.Item>
                         </Col>
                         <Col>
-                            <Form.Item label="Zip Code" style={{ width: "320px" }}>
+                            <Form.Item  name="zip code" rules={[{ required: true}]} style={{ width: "320px" }}>
+                                <label>Zip Code</label>
                                 <Input placeholder="500000" />
                             </Form.Item>
                         </Col>
-                        <Button
-                            htmlType="button"
-                            onClick={this.onSave}
-                        >
-                            Save
-                        </Button>
                     </Row>
+                    <Form.Item>
+                    <Button
+                        htmlType="Submit"
+                        type="primary"
+                    >
+                        Save
+                    </Button>
+                    </Form.Item>                
                 </Form>
             </>
         )
