508e274526d711d273122531949fe595edf571db
Nandakishore Vishwanath <104244494+Nandakishore695@users.noreply.github.com>
2022-08-30 12:47:56 +0530

    Update App.js

--- Antd-Project/src/App.js
+++ Antd-Project/src/App.js
@@ -1,28 +1,12 @@
-import { useEffect, useState } from "react";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import Auth from "./auth";
-import AppLayout from "./layout";
-import { store } from "./store";
-import { loadUser, OidcProvider } from 'redux-oidc';
-import { userManager } from "./auth/index";
-function App() {
-  const [isLoggedIn, setLoggedIn] = useState(false);
-  useEffect(() => {
-    localStorage.setItem("__url", window.location.pathname);
-    loadUser(store, userManager).then(user => {
-      setLoggedIn(true);
-    })
-  }, [])
-  return (
-    <Provider store={store}>
-      <OidcProvider userManager={userManager} store={store}>
-      <BrowserRouter>
-        {isLoggedIn ? <AppLayout /> : <div className="loader">Loading....</div>}
-      </BrowserRouter>
-      </OidcProvider>
-    </Provider>
-  );
-}
+import 'antd/dist/antd.css';
+import GridFormCom from "./Project/GridForm";
 
+function App() {
+
+  return (
+    <>
+       <GridFormCom/>
+    </>
+  )
+}
 export default App;
