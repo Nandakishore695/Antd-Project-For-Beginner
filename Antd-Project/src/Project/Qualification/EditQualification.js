2411eb7fe2e34c645bd39ef638c0515efcbe73be
Nandakishore Vishwanath <104244494+Nandakishore695@users.noreply.github.com>
2022-08-31 16:37:02 +0530

    Add files via upload

---
+++ Antd-Project/src/Project/Qualification/EditQualification.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { EditOutlined } from '@ant-design/icons';
+import { Modal, Select, Form } from 'antd'
+const { Option } = Select;
+
+function EditQualificationPopup() {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleOk = () => {
+        setIsModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+
+    return (
+        <>
+            <EditOutlined type="primary" onClick={showModal} style={{ color: "green", fontSize: 20 }} />
+            <Modal title="Edit Qualification" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                <p><b> Selection Type</b></p>
+                <Form>
+                    <Form.Item>
+                        <label>Degree</label>
+                        <Select
+                            placeholder="Select a option"
+                            style={{ width: "150px" }}
+                        >
+                            <Option value="residential">B.Sc</Option>
+                            <Option value="rental">B.A</Option>
+                            <Option value="rental">B.Com</Option>
+                            <Option value="rental">B.B.A</Option>
+                            <Option value="rental">B.Ed</Option>
+                        </Select>
+                    </Form.Item>
+                    <Form.Item>
+                        <label>B.Tech</label>
+                        <Select
+                            placeholder="Select a option"
+                            style={{ width: "150px" }}
+                        >
+                            <Option value="residential">B.Sc</Option>
+                            <Option value="rental">Civil</Option>
+                            <Option value="rental">Mechnical</Option>
+                            <Option value="rental">Electrical</Option>
+                            <Option value="rental">Electronic</Option>
+                        </Select>
+                    </Form.Item>
+                </Form>
+            </Modal>
+        </>
+    );
+};
+export default EditQualificationPopup;
