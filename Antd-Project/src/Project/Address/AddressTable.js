b5e59838a0b74589865ab87aed03ada5567499c1
Nandakishore Vishwanath <104244494+Nandakishore695@users.noreply.github.com>
2022-09-03 08:12:04 +0530

    Add files via upload

---
+++ Antd-Project/src/Project/Address/AddressTable.js
@@ -0,0 +1,48 @@
+import {Table } from 'antd';
+import React, { useState } from 'react';
+
+const columns = [
+  {
+    title: 'Type',
+    dataIndex: 'residential',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'degree',
+  },
+  {
+    title: 'Zip Code',
+    dataIndex: 'degree',
+  }
+];
+const data = [];
+
+for (let i = 0; i < 4; i++) {
+  data.push({
+    key: i,
+    name: `Edward King ${i}`,
+    age: 32,
+    address: `London, Park Lane no. ${i}`,
+  });
+}
+
+const GridTable = (props) => {
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+  const onSelectChange = (newSelectedRowKeys) => {
+    console.log('selectedRowKeys changed: ', selectedRowKeys);
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
+
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+  };
+
+  return (
+    <div>
+      <Table rowSelection={rowSelection} columns={columns} dataSource={props.data} style={{width:"200px"}} />
+    </div>
+  );
+};
+
+export default GridTable;
